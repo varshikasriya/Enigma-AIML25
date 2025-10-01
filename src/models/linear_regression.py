12c94b7c9f626817e4f18a0c5271e4b5a0633816
Dhruv N <tankig912@gmail.com>
2025-10-01 23:17:23 +0530

    Added docstring and weight init

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -6,12 +6,24 @@ from .base_model import BaseModel
 from typing import Optional, Dict, Union
 
 class LinearRegression(BaseModel):
+    """Simple Linear Regression model."""
     def __init__(
         self,
         in_features: int,
         out_features: int,
         bias_term: bool = True,
     ) -> None:
+        """
+        Initializes the LinearRegression model.
+        Weights and bias are initialized during forward pass (if not already).
+
+        Args:
+            in_features (int): Number of input features.
+            out_features (int): Number of output features.
+            bias_term (bool): If True, includes a bias term in the model.
+        Returns:
+            None
+        """
         super().__init__(_forward_has_training_logic=False)
         self.in_features = in_features
         self.out_features = out_features
@@ -26,9 +38,17 @@ class LinearRegression(BaseModel):
         loss_fn: Optional[callable] = None,
         print_logs: bool = False,
         return_logs: bool = False,
-    ) -> Union[Dict[str, list], np.ndarray]:
+    ) -> Dict[str, Union[float, list, np.ndarray]]:
         """
-        Perform forward pass and compute loss,
+        Perform forward pass and compute loss.
+        If in training mode (self._training is True), performs training logic.
+        If in evaluation mode (self._training is False), performs inference logic.
+        The model's weights and bias are initialized during the first forward pass if they are None.
+
+        Known issue(s):
+        - Model's weight initialization is not deterministic
+        - Weight initialization should ideally be done in __init__
+        - Initialized weights tend to be large, causing instability during optimization
 
         Args:
             Xb (np.ndarray): Input batch.
@@ -37,8 +57,15 @@ class LinearRegression(BaseModel):
             print_logs (bool): Print logs if True.
             return_logs (bool): Return logs if True.
         Returns:
-            Optional[Dict[str, list]]: Logs if return_logs is True.
+            Dict[str, Union[float, list, np.ndarray]]: Dict containing loss, predictions, and other logs (if training).
         """
+        # weight init to be done
+        if self.weights is None:
+            self.weights = np.random.randn(self.in_features, self.out_features)
+        if self.bias_term and self.bias is None:
+            self.bias = np.zeros((1, self.out_features))
+
+        out = {}  # return dict
         if self._training:
             # training logic here, like:
             # weight init and bias init (if bias_term is True)
@@ -52,6 +79,5 @@ class LinearRegression(BaseModel):
         else:
             # inference logic here
             pass
-        return None
-        # return the predictions
-
+        
+        return out
---
+++ src/models/torch_mlp.py
@@ -0,0 +1,4 @@
+# Need to implement this file for TorchMLP model
+# The model should inherit form nn.Module ONLY, NOT from BaseModel
+# The training logic should be implemented outside the model, in the training script
+# Torch models are trained using torch's API and optimizers.
