6be598860a44a00fd960618f98d8c3ee392ebbff
kpsr01 <pranavsuhasreddykonduru@gmail.com>
2025-10-21 11:14:50 +0530

    added decision tree classifier

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,10 +2,12 @@ from .base_model import BaseModel
 from .linear_regression import LinearRegression
 from .perceptron import Perceptron
 from .logistic_regression import LogisticRegression
+from .decision_tree_classifier import DecisionTreeClassifier
 
 __all__ = [
     "BaseModel",
     "LinearRegression",
     "Perceptron",
     "LogisticRegression",
+    "DecisionTreeClassifier",
 ]
