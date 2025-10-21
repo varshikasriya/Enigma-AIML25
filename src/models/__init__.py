585cda9c09f98798a6bbb8a632946d116e460d6b
kpsr01 <pranavsuhasreddykonduru@gmail.com>
2025-10-21 11:07:12 +0530

    added logistic regression

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,11 @@
 from .base_model import BaseModel
 from .linear_regression import LinearRegression
 from .perceptron import Perceptron
+from .logistic_regression import LogisticRegression
 
 __all__ = [
     "BaseModel",
     "LinearRegression",
     "Perceptron",
+    "LogisticRegression",
 ]
