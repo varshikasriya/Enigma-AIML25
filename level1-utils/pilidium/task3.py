aa95f850c168359e0f7d777e7c00b0d62251d041
pilidium <147639261+pilidium@users.noreply.github.com>
2025-10-24 06:20:46 +0530

    Rectified missing sqrt in norm_l2

--- level1-utils/pilidium/task3.py
+++ level1-utils/pilidium/task3.py
@@ -1,3 +1,5 @@
+import math
+
 def norm_l1(x : list[int | float]) -> list[float]:
     "Returns L1 normalised x. This means the sum of absolute values of its elements will be 1."
     absum = sum(abs(i) for i in x)
@@ -5,7 +7,7 @@ def norm_l1(x : list[int | float]) -> list[float]:
 
 def norm_l2(x : list[int | float]) -> list[float]:
     "Returns L2 normalised x. This means the square root of the sum of squares of its elements will be 1."
-    sqsum = sum(i*i for i in x)
+    sqsum = math.sqrt(sum(i*i for i in x))
     return [i/sqsum for i in x]
 
 def norm_min_max(x : list[int | float]) -> list[float]:
@@ -25,6 +27,6 @@ if __name__ == "__main__":
     """
     Output:
     [-0.1411764705882353, 0.47058823529411764, 0.0, 0.388235294117647]
-    [-0.04235792446170138, 0.1411930815390046, 0.0, 0.11648429226967878]
+    [-0.22545400718115802, 0.7515133572705267, 0.0, 0.6199985197481845]
     [0.0, 1.0, 0.23076923076923075, 0.8653846153846153]
     """
